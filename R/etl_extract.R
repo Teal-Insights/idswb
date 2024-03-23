6e3aa04ece93e414185de879e1f118a1bab061ca
Reu-hub <opondoreuben94@gmail.com>
2024-03-23 19:42:05 +0300

    updates

--- R/etl_extract.R
+++ R/etl_extract.R
@@ -13,10 +13,22 @@
 #' @param ... arguments passed to \code{\link{download.file}}. Set method as "curl" for Linux system, and as default for Windows and Mac OS X systems.
 #' @details This function downloads idswb data from idswb OPEN DATA website for years and months specified.
 etl_extract.etl_idswb <- function(obj, ...) {
+  # message
   message("Extracting raw data...")
-  raw_filename <- file.path(attr(obj, "raw_dir"), "mtcars.csv")
-  datasets::mtcars %>%
-    tibble::rownames_to_column("model") %>%
-    readr::write_csv(file = raw_filename)
+
+  # loading data
+  ids_full_data <- readRDS("./inst/extdata/ids_full_data.rds")
+
+  # loop each file in the list
+  ids_files <- names(ids_full_data)
+  ids_file_names = file.path(attr(obj, "raw_dir"), paste0(ids_files,".csv"))
+
+  for (i in seq_along(ids_files)) {
+    # subset data
+    ids_data <- ids_full_data[[i]]
+    # export raw data
+    readr::write_csv(x = ids_data, file = paste0("./data-raw/",ids_file_names[i]))
+  }
+  # invisible
   invisible(obj)
 }
