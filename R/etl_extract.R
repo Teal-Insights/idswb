0061fa6d4f1f73c26166173cafb28ff74e799c16
Reu-hub <opondoreuben94@gmail.com>
2024-03-24 00:00:43 +0300

    updates

--- R/etl_extract.R
+++ R/etl_extract.R
@@ -1,21 +1,18 @@
 #' etl_extract
-#' @export
 #' @rdname etl_extract.etl_idswb
 #' @method etl_extract etl_idswb
-#' @import dplyr
 #' @import etl
+#' @import dplyr
+#' @importFrom rvest html_nodes html_text
+#' @importFrom xml2 read_html
 #' @importFrom utils download.file
-#' @importFrom lubridate year month
+#' @importFrom lubridate year month days parse_date_time
 #' @inheritParams etl::etl_extract
-#' @param years years that the issues have been created (the most recent year is the default)
-#' @param months months that the issues have been created (the most recent month is the default)
-#' @param num_calls number of readings (1000000 is the default)
-#' @param ... arguments passed to \code{\link{download.file}}. Set method as "curl" for Linux system, and as default for Windows and Mac OS X systems.
-#' @details This function downloads idswb data from idswb OPEN DATA website for years and months specified.
+#' @details This function downloads International Debt statistics data
+#' in the raw directory under the folder the user created
+#' @export
 etl_extract.etl_idswb <- function(obj, ...) {
-  # message
   message("Extracting raw data...")
-
   # loading data
   ids_full_data <- readRDS("./inst/extdata/ids_full_data.rds")
 
