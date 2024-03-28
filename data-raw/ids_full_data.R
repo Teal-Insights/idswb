0db31055a1b76267fae4c988c316a3902c60f66f
Reu-hub <opondoreuben94@gmail.com>
2024-03-28 18:55:47 +0300

    updates

--- data-raw/ids_full_data.R
+++ data-raw/ids_full_data.R
@@ -9,20 +9,23 @@ excel_files <- list.files(path = '../../../../data/ids', pattern = ".xlsx")
 # read excel files --------------------------------------------------------
 excel_list <- list()
 for (i in seq_along(excel_files)) {
+  # pop up message
   message(paste0("Reading ",excel_files[i]))
-  excel_list[[i]] <- read_excel(excel_files[i])
+  # reading files
+  excel_list[[i]] <- readxl::read_excel(path = paste0("../../../../data/ids/",excel_files[i]))
 }
 
-df_full <- rbindlist(excel_list)
+# row bind a list of dataframes
+df_full <- data.table::rbindlist(excel_list)
 
 # metadata
-unique_counterpart_area <- read_csv(file = "unique_counterpart_area.csv")
-unique_country <- read_csv(file = "unique_country.csv")
-unique_series <- read_csv(file = "unique_series.csv")
-unique_time <- read_csv(file = "unique_time.csv")
+unique_counterpart_area <- readr::read_csv(file = paste0("../../../../data/ids/", "unique_counterpart_area.csv"))
+unique_country <- readr::read_csv(file = paste0("../../../../data/ids/", "unique_country.csv"))
+unique_series <- readr::read_csv(file = paste0("../../../../data/ids/", "unique_series.csv"))
+unique_time <- readr::read_csv(file = paste0("../../../../data/ids/", "unique_time.csv"))
 
 # list of dataframes
-ids_list = list(
+ids_full_data = list(
   ids_wb = df_full,
   unique_counterpart_area = unique_counterpart_area,
   unique_country = unique_country,
@@ -30,7 +33,8 @@ ids_list = list(
   unique_time = unique_time
 )
 
-usethis::use_data(DATASET, overwrite = TRUE)
+# exporting data into data folder
+usethis::use_data(ids_full_data, overwrite = TRUE,compress = "xz")
 
 # ends: -------------------------------------------------------------------
 
