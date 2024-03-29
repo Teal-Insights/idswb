3bc9499c69cc72e28bd0170ea6b20d542c319720
Reu-hub <opondoreuben94@gmail.com>
2024-03-29 22:57:05 +0300

    updates

--- data-raw/ids_full_data.R
+++ data-raw/ids_full_data.R
@@ -34,10 +34,10 @@ ids_full_data = list(
 )
 
 # exporting to ins/extdata directory
-base::save(ids_full_data,file = "inst/extdata/ids_full_data2.rda")
+base::saveRDS(ids_full_data,file = "inst/extdata/ids_full_data2.rds")
 
 # exporting data into data folder
-usethis::use_data(ids_full_data, overwrite = TRUE,compress = "xz",version = 3)
+usethis::use_data(ids_full_data, overwrite = TRUE,compress = "xz")
 
 # ends: -------------------------------------------------------------------
 
