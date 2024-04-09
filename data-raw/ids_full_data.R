80cd2396eb38aa4f20eba189f8a0ca1fd136e958
Reu-hub <opondoreuben94@gmail.com>
2024-04-09 19:04:30 +0300

    updates

--- data-raw/ids_full_data.R
+++ data-raw/ids_full_data.R
@@ -5,16 +5,18 @@ library(magrittr)
 
 # loading data: -----------------------------------------------------------
 # main data
-ids <- readr::read_rds(file = "../../../../data/ids/ids_data_released_2024-02-29.rds")
-
+ids <- readr::read_rds(file = "../../../../data/ids/data/ids_data_released_2024-02-29.rds")
+ids_one <- ids[1:1750000,]
+ids_two <- ids[-c(1:1750000),]
 # metadata
-unique_counterpart_area <- readr::read_csv(file = paste0("../../../../data/ids/", "unique_counterpart_area.csv"))
-unique_country <- readr::read_csv(file = paste0("../../../../data/ids/", "unique_country.csv"))
-unique_series <- readr::read_csv(file = paste0("../../../../data/ids/", "unique_series.csv"))
-unique_time <- readr::read_csv(file = paste0("../../../../data/ids/", "unique_time.csv"))
+unique_counterpart_area <- readr::read_csv(file = paste0("../../../../data/ids/data/", "unique_counterpart_area.csv"))
+unique_country <- readr::read_csv(file = paste0("../../../../data/ids/data/", "unique_country.csv"))
+unique_series <- readr::read_csv(file = paste0("../../../../data/ids/data/", "unique_series.csv"))
+unique_time <- readr::read_csv(file = paste0("../../../../data/ids/data/", "unique_time.csv"))
 
 # exporting data into data folder
-usethis::use_data(ids, overwrite = TRUE,compress = "xz")
+usethis::use_data(ids_one, overwrite = TRUE,compress = "xz")
+usethis::use_data(ids_two, overwrite = TRUE,compress = "xz")
 usethis::use_data(unique_counterpart_area, overwrite = TRUE,compress = "xz")
 usethis::use_data(unique_country, overwrite = TRUE,compress = "xz")
 usethis::use_data(unique_series, overwrite = TRUE,compress = "xz")
